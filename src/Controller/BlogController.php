fee16a5cf0085a20e993673d2c5f4987b779eba7
CobaltProg <Rativeau.corentin@gmail.com>
2020-09-22 00:32:32 +0200

    modification front pour le blog geemka

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,16 +4,42 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
+    public function index(ArticleRepository $repo)
     {
+    
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
+    /**
+     * @Route("/", name="home")
+     */
+    public function home() {
+        return $this->render('blog/home.html.twig');
+    }
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     */
+    public function show(Article $article) {
+       
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
+    }
+
+    public function create() {
+        return $this->render('blog/create.html.twig');
+    }
+
 }
