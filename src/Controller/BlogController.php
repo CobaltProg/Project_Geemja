d48a7b01134de9669e40d1213e1bb71405cf7712
CobaltProg <Rativeau.corentin@gmail.com>
2020-09-22 21:14:00 +0200

    Form

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,6 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
 
@@ -22,12 +28,54 @@ class BlogController extends AbstractController
             'articles' => $articles
         ]);
     }
+
     /**
      * @Route("/", name="home")
      */
     public function home() {
         return $this->render('blog/home.html.twig');
     }
+
+    /**
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
+     */
+    public function form(Article $article = null, Request $request, EntityManagerInterface $manager) {
+
+        if(!$article) {
+            $article = new Article();
+        }
+       
+
+        $article->setTitle("Titre d'exemple")
+                ->setContent("Le contenue de l'article");
+
+        $form = $this->createFormBuilder($article)
+                    ->add('title')
+                    ->add('content')
+                    ->add('image')
+                    ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            if(!$article->getId()){
+                $article->setCreatedAt(new \DateTime());
+            }
+            
+
+            $manager->persist($article);
+            $manager->flush();
+            
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createview(),
+            'editMode' => $article->getId() !== null
+        ]);
+    }
+
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
@@ -37,9 +85,6 @@ class BlogController extends AbstractController
             'article' => $article
         ]);
     }
-
-    public function create() {
-        return $this->render('blog/create.html.twig');
-    }
+    
 
 }
